863e379cf284f31db8c77f78088e12f1272ed2e2
Kiogra <89411673+Kiogra@users.noreply.github.com>
2022-12-10 23:41:36 +0100

    BOT

--- index.js
+++ index.js
@@ -127,44 +127,6 @@ client.on('guildMemberRemove', member => {
   channel5.send({embeds: [embed]})
 }); 
 
-
-
-//New Story
-let date = new Date()
-if (date.getHours() == 0 && date.getMinutes() == 0 && date.getSeconds() == 0) {
-    let serverstats = getServer()
-
-    let embed = new Discord.MessageEmbed()
-        .setTitle(":orange_book: Storia terminata")
-        .setColor(colors.purple)
-        .setDescription("Il tempo per la storia di oggi è **terminato**, ecco tutto quello che avete scritto")
-        .addField(":bar_chart: Info", `
-        Parole scritte: ${serverstats.onewordstory.totWordsToday}
-        Utenti unici partecipanti: ${serverstats.onewordstory.words.filter((v, i, a) => a.findIndex(x => x.user == v.user) == i).length}`)
-
-    let story = `-- STORIA DEL ${moment().format("DD-MM-YYYY")} --\n\n`
-    story += serverstats.onewordstory.words.map(x => x.word).join(" ")
-
-    const attachment = new Discord.MessageAttachment(Buffer.from(story, "utf-8"), `story-${moment().format("DDMMYYYY")}.txt`);
-
-    client.channels.cache.get(settings.idCanaliServer.onewordstory).send({ embeds: [embed], files: [attachment] })
-        .then(msg => msg.pin())
-
-    serverstats.onewordstory.words = []
-    serverstats.onewordstory.totWordsToday = 0
-    serverstats.onewordstory.totStories++
-
-    updateServer(serverstats)
-
-    let userstatsList = getAllUsers(client)
-    userstatsList.forEach(userstats => {
-        if (userstats.onewordstory.totWordsToday != 0) {
-            userstats.onewordstory.totWordsToday = 0;
-            updateUser(userstats)
-        }
-    })
-}
-
 // spawn_python.js
 var util = require("util");
 
